4b4fb96310ec6a11e254369be1fbfce4bb6b270e
Hang Su <hang.su@algorand.com>
2022-01-28 15:13:19 -0500

    update minor on method return

--- pyteal/ast/abi/methodret.py
+++ pyteal/ast/abi/methodret.py
@@ -1,10 +1,11 @@
 from typing import TYPE_CHECKING
 
-from ...ast.naryexpr import Concat
-from ...ast.unaryexpr import Log
-from ...errors import TealCompileError, verifyTealVersion
+from ..naryexpr import Concat
+from ..unaryexpr import Log
+from ..subroutine import SubroutineCall
+from ...errors import TealCompileError, TealInputError, verifyTealVersion
 from ...ir.ops import Op
-from ...types import TealType, types_match
+from ...types import TealType
 from ...config import RETURN_EVENT_SELECTOR
 from ..expr import Expr
 
@@ -15,16 +16,24 @@ if TYPE_CHECKING:
 class MethodReturn(Expr):
     def __init__(self, value: Expr) -> None:
         super().__init__()
+        if not isinstance(value, SubroutineCall):
+            raise TealInputError(
+                "method return can only be applied to handle method handler subroutine"
+            )
+        if not value.subroutine.getDeclaration().has_return():
+            raise TealInputError(
+                "method return do not accept method handler (subroutine) with no return"
+            )
         self.value = value
 
     def __teal__(self, options: "CompileOptions"):
-        if types_match(self.value.type_of(), TealType.bytes):
+        if self.value.type_of() == TealType.bytes:
             verifyTealVersion(
                 Op.log.min_version, options.version, "TEAL version too low to use log"
             )
             concatLogExpr = Log(Concat(RETURN_EVENT_SELECTOR, self.value))
             return concatLogExpr.__teal__(options=options)
-        elif types_match(self.value.type_of(), TealType.none):
+        elif self.value.type_of() == TealType.none:
             return self.value.__teal__(options=options)
         else:
             raise TealCompileError(
---
+++ pyteal/ast/abi/methodret_test.py
@@ -0,0 +1,41 @@
+import pytest
+
+from ... import *
+
+options = CompileOptions(version=5)
+
+
+def test_method_return_subroutine_log():
+    @Subroutine(TealType.bytes, "ork()byte[13]")
+    def ork() -> Expr:
+        return Return(Bytes("gork and mork"))
+
+    logReturn = abi.MethodReturn(ork())
+
+
+def test_method_return_subroutine_void():
+    @Subroutine(TealType.none, "kiwibird(byte[8])void")
+    def kiwibird(worm: Expr):
+        return Return()
+
+    eatWorm = abi.MethodReturn(kiwibird(Bytes("iamaworm")))
+
+
+def test_method_return_subroutine_no_ret():
+    pass
+
+
+def test_method_return_subroutine_wrong_type():
+    @Subroutine(TealType.uint64, "badNumber(uint64)uint64")
+    def badNumber(a: Expr) -> Expr:
+        return Return(Int(0xDEADBEEF) + a)
+
+    with pytest.raises(TealCompileError):
+        abi.MethodReturn(badNumber(Int(114514))).__teal__(options=options)
+
+    @Subroutine(TealType.anytype, "fakeUint64()byte[8]")
+    def fakeUint64() -> Expr:
+        return Return(Bytes("fakeuint"))
+
+    with pytest.raises(TealCompileError):
+        abi.MethodReturn(fakeUint64()).__teal__(options=options)
